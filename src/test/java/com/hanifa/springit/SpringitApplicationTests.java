3f5620e961ad8e4be90f6a81f5e944d13e8aed53
hani <hani@mystiquex.com>
2020-06-22 07:23:20 -0400

    Initial commit for springboot reddit clone

---
+++ src/test/java/com/hanifa/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanifa.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
